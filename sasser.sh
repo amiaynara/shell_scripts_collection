f474b425c616c369d71afa780b66257ca70c008c
Amiay Narayan <amiayn@iitbhilai.ac.in>
2020-06-30 06:20:02 +0530

    it works

--- sasser.sh
+++ sasser.sh
@@ -2,15 +2,24 @@
 
 #should execute only if there is an argument else exit. 
 
+if [ $# ==  0 ]
+then
+  echo "Name of the project NEEDED!!!"
+else  
 #create main project folder
-mkdir $1/{.sass-cache,SASS{BASE,LAYOUT,MODULE,STATE,THEME},CSS}
-cd $1/SASS
-touch BASE/_base-dir.sass LAYOUT/_layout-dir.sass MODULE/_module-dir.sass STATE/_state-dir.sass THEME/_theme-dir.sass app.sass # done for creating partials
-cd ..
-touch $1/CSS/app.css
-touch $1/index.html
+  mkdir $1
+  mkdir -p $1/{.sass-cache,SASS/{BASE,LAYOUT,MODULE,STATE,THEME},CSS}
+  cd $1/SASS
+  touch BASE/_base-dir.sass LAYOUT/_layout-dir.sass MODULE/_module-dir.sass STATE/_state-dir.sass THEME/_theme-dir.sass app.sass # done for creating partials
+  cd ..
+  touch CSS/app.css
+  touch index.html
+
 #showing the structure created. 
-tree .
+  tree .
+
+  echo "STRUCTURE successfully created!!!"
+fi
 
 # we can add -w flag for --watch 
 # we can also add -x flag for removing an existing system of SMACSS structure
