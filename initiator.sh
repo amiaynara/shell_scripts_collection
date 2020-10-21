dbcdc5358bbd515c2fa8968d17ad4bd262dc9702
Amiay Narayan <amiayn@iitbhilai.ac.in>
2020-10-21 09:27:04 +0530

    use single quotes while using ) in file name

--- initiator.sh
+++ initiator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #I can create a youtube video about this
-cd /home/tesla/learn/Udemy/Machine_learning/Machine+Learning+A-Z+(Codes+and+Datasets) #----> opens this directory
+cd /home/tesla/learn/Udemy/Machine_learning/'Machine+Learning+A-Z+(Codes+and+Datasets)' #----> opens this directory
 terminator 
 clear
